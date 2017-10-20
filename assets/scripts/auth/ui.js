ce9bb616c7528ef4d022ccf61cd7faa67cc89b7e
hilarybrown <hilary.brown@libertymutual.com>
2017-10-20 16:13:49 -0400

    Add html and ui for showing GetAllMovies button

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -18,7 +18,7 @@ const signUpFailure = function (error) {
 // show success message
 const signInSuccess = function (data) {
   console.log(data)
-  $('$getMoviesButton').show()
+  $('#getMoviesButton').show()
   $('#signInModal').modal('hide')
   $('#sign-in-container').hide()
   $('#pwUpdateContainer').show()
@@ -45,7 +45,7 @@ const changePasswordFailure = function (error) {
 }
 
 const signOutSuccess = function (data) {
-  $('$getMoviesButton').hide()
+  $('#getMoviesButton').hide()
   $('#pwUpdateContainer').hide()
   $('#sign-out').hide()
   $('#newMovieModal').hide()
